51eb1ca561572325a0c3bc181e431897dd6b7adb
Jalena Correa <95106060+jcorreaan@users.noreply.github.com>
2022-06-15 19:57:45 -0500

    ok

--- pregunta_02/reducer.py
+++ pregunta_02/reducer.py
@@ -1,3 +1,29 @@
 #
-# >>> Escriba el codigo del reducer a partir de este punto <<<
+# >>> Escriba el codigo del mapper a partir de este punto <<<
 #
+import sys
+
+if __name__ == "__main__":
+
+    curkey=None
+    max=0
+
+    for line in sys.stdin:
+
+        row=line.split("\t")
+        key=row[0]
+        value=int(row[1])
+
+        if curkey==key:
+            if value>max:
+                max=value
+            else:
+                value=max
+        else:
+            if curkey is not None:
+                sys.stdout.write("{}\t{}\n".format(curkey, max))
+
+            curkey=key
+            max=value
+
+    sys.stdout.write("{}\t{}\n".format(curkey, max))
