56e742175d07520560ab6e16ab8aa4da01b7302a
Jalena Correa <95106060+jcorreaan@users.noreply.github.com>
2022-06-25 08:19:19 -0500

    ok

--- pregunta_01/mapper.py
+++ pregunta_01/mapper.py
@@ -1,3 +1,15 @@
 #
 # >>> Escriba el codigo del mapper a partir de este punto <<<
 #
+
+
+import sys
+
+if __name__ == "__main__":
+
+     wordx[2]
+
+    for line in sys.stdin:
+       word  = line.split(',')
+   sys.stdout.write("{}\t{}\n".format(word))
+
